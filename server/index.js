d436d60080bd00a62d344ba0a535bf2f2a9fbf52
Sanatan Barick <bsanatan975@gmail.com>
2026-04-19 23:37:04 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ import paymentRouter from "./routs/payment.rout.js";
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://interview-iq-client.onrender.com",
     credentials:true
 }))
 
